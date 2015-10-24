287b704ee9fdf3d5d22b97897f63b59dc6cc4788
Tony Edwards <tony@plymouthsoftware.com>
2015-10-25 00:47:33 +0100

    Adds pannels and stats to interface

--- app/js/main.js
+++ app/js/main.js
@@ -12,15 +12,48 @@ var populateUI = function(data) {
   earthQuakes.pop();
 
   displayLargestQuake();
-  displayData();
+  // displayData();
+  displayStats();
 };
 
 var displayLargestQuake = function(){
-  $("#largestQuake").append("BIGGEST RECENT QUAKE: " + Earthquake.getLargestQuake(earthQuakes));
+  let description = Earthquake.getLargestQuake(earthQuakes).getDescription();
+  $("#largestQuake").append(`BIGGEST RECENT QUAKE: ${description}`);
 };
 
 var displayData = function(){
   for(var i = 0; i < earthQuakes.length; i++) {
     $("#output").append(earthQuakes[i].getDescription() + '<br />');
   }
+};
+
+var displayStats = function() {
+  var sevenDays = earthQuakes.length;
+  document.getElementById('seven-days').innerHTML = sevenDays;
+
+  // get date 24hours ago
+  var twentyFourHours = new Date();
+  twentyFourHours.setDate(twentyFourHours.getDate()-1);
+
+
+  var oneHour = new Date();
+  oneHour.setDate(oneHour.getDay()-1);
+
+  // loop earthquake
+  var dayOfQuakes = [];
+  var hourOfQuakes = [];
+  earthQuakes.forEach(function(quake) {
+    var quakeDate = new Date(quake.time);
+    console.log(quakeDate);
+    if (quakeDate >= oneHour) {
+      hourOfQuakes.push(quake);
+    }
+    if (quakeDate >= twentyFourHours){
+      dayOfQuakes.push(quake);
+    }
+  });
+
+  document.getElementById('one-day').innerHTML = dayOfQuakes.length;
+
+  document.getElementById('one-hour').innerHTML = hourOfQuakes.length;
 };
