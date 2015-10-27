90063174dc07a13709aa0f411f114bb3fad05775
Tony Edwards <tony@plymouthsoftware.com>
2015-10-27 18:07:08 +0000

    Indent code

--- app/js/main.js
+++ app/js/main.js
@@ -13,34 +13,34 @@ $(document).ready(function(){
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], 0.1);
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], 0.1);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+  var y = d3.scale.linear()
+      .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "%");
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10, "%");
 
-var svg = d3.select("#quake-bar-chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select("#quake-bar-chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", type, function(error, data) {
-  if (error) throw error;
+  d3.tsv("data.tsv", type, function(error, data) {
+    if (error) throw error;
 
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
-  svg.append("g")
+    svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
@@ -136,6 +136,7 @@ var populateUI = function(data) {
   displayLargestQuake();
   displayStats();
   buildMap(earthQuakes);
+  earthQuakeData.getEarthQuakesPerDay();
 };
 
 var displayLargestQuake = function(){
