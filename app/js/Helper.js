c1f1f81f3eb3306d0a076d698e8aa6d6dcb9be61
Tony Edwards <tony@plymouthsoftware.com>
2015-11-04 19:19:20 +0000

    Adds method for calculating distance between two points

--- app/js/Helper.js
+++ app/js/Helper.js
@@ -5,6 +5,25 @@ class Helper {
 
   }
 
+  static comapreDistance(lat1, lng1, lat2, lng2) {
+      var radlat1 = Math.PI * lat1 / 180;
+      var radlat2 = Math.PI * lat2 / 180;
+      var radlon1 = Math.PI * lng1 / 180;
+      var radlon2 = Math.PI * lng2 / 180;
+      var theta = lng1 - lng2;
+      var radtheta = Math.PI * theta / 180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      dist = Math.acos(dist);
+      dist = dist * 180 / Math.PI;
+      dist = dist * 60 * 1.1515;
+
+      //Get in in kilometers
+      dist = dist * 1.609344;
+
+      // console.log(dist);
+      return dist;
+  }
+
   static CSVToArray(strData, strDelimiter) {
       // Check to see if the delimiter is defined. If not,
       // then default to comma.
