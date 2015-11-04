15697d846037cd8f02e0aa0c0e2733c2941d3a9f
Tony Edwards <tony@plymouthsoftware.com>
2015-11-04 22:19:49 +0000

    Adds creation and passing of a user object.
    The object contains the location of the user, but is suseptable to race conditions.

--- app/js/User.js
+++ app/js/User.js
@@ -1,31 +1,8 @@
 'use strict';
 
 class User {
-  constructor() {
-    this.handlers = [];
-    this.setLocation();
-  }
-
-  setLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(geo){
-        console.log(geo);
-        this.latitude = geo.coords.latitude;
-        this.longitude = geo.coords.longitude;
-        console.log(this.latitude);
-      });
-    } else {
-      console.log("Set Location: No geo available");
-    }
-  }
-
-  getLocation() {
-    if (typeof this.latitude != 'undefined') {
-      // fallback
-      return [this.latitude, this.longitude];
-
-    } else {
-      console.log("There is no location");
-    }
+  constructor(location) {
+    this.latitude = location[0];
+    this.longitude = location[1];
   }
 }
